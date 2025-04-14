dbfad46cc6ce578596b53fb967f0e875b2e55f77
yloose <yanloose@gmail.com>
2025-04-14 17:32:11 +0200

    Add Userinfo

--- src/main/java/de/cau/studidbv2/service/StudidbService.java
+++ src/main/java/de/cau/studidbv2/service/StudidbService.java
@@ -1,6 +1,7 @@
 package de.cau.studidbv2.service;
 
 import de.cau.studidbv2.dto.ExamResult;
+import de.cau.studidbv2.dto.StudidbUserInfo;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -8,7 +9,6 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -20,11 +20,12 @@ import java.util.stream.IntStream;
 public class StudidbService {
 
     private final Logger LOG = LoggerFactory.getLogger(StudidbService.class);
+    private static final String STUDIDB_BASE_URL = "https://studidb.informatik.uni-kiel.de:8484";
 
     public StudidbAuthorization login(String username, String password) throws LoginException {
         Connection.Response res;
         try {
-            res = Jsoup.connect("https://studidb.informatik.uni-kiel.de:8484/studierende/login")
+            res = Jsoup.connect(STUDIDB_BASE_URL + "/studierende/login")
                     .userAgent("Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0")
                     .header("Cache-Control", "no-cache")
                     .data("username", username, "password", password, "login", "Login")
@@ -52,7 +53,7 @@ public class StudidbService {
         return new StudidbAuthorization(sessionId, jsessionId);
     }
 
-    public List<ExamResult> parseResults(Document doc) throws Exception {
+    private List<ExamResult> parseExamResults(Document doc) throws Exception {
         Element table = doc.selectFirst(".standard2");
         if (table == null)
             throw new Exception("Received unexpectedly formatted response");
@@ -74,13 +75,34 @@ public class StudidbService {
                 ).collect(Collectors.toList());
     }
 
-    public Document getExamResultsDocument(StudidbAuthorization authorization) throws IOException {
+    private StudidbUserInfo parseUserInfo(Document doc) throws Exception {
+        Element table = doc.selectFirst("table.standard");
+        if (table == null)
+            throw new Exception("Received unexpectedly formatted response");
 
-        Connection.Response res = Jsoup.connect("https://studidb.informatik.uni-kiel.de:8484/studierende/leistungen?session_id=" + authorization.sessionId())
+        Elements generalAttributes = table.select("p b");
+        if (generalAttributes.size() < 3)
+            throw new Exception("Received unexpectedly formatted response");
+        String name = generalAttributes.get(0).text();
+        String street = generalAttributes.get(1).text();
+        String city = generalAttributes.get(2).text();
+
+        String phone = table.select("input[name=telefon]").attr("value");
+
+        Elements emailContainer = table.select("tr:has(td:contains(E-Mail))").select("td");
+        if (emailContainer.size() < 2)
+            throw new Exception("Received unexpectedly formatted response");
+        String email = emailContainer.get(1).text();
+
+        return new StudidbUserInfo(name, street + ", " + city, phone, email);
+    }
+
+    private Document getStudidbDocument(String path, String sessionId, String jsessionId) throws IOException {
+        Connection.Response res = Jsoup.connect(STUDIDB_BASE_URL + path + "?session_id=" + sessionId)
                 .userAgent("Mozilla/5.0 (X11; Linux x86_64; rv:136.0) Gecko/20100101 Firefox/136.0")
                 .header("Cache-Control", "no-cache")
-                .header("Referer", "https://studidb.informatik.uni-kiel.de:8484/studierende/start?session_id=" + authorization.sessionId())
-                .cookie("JSESSIONID", authorization.jsessionId())
+                .header("Referer", STUDIDB_BASE_URL + "/studierende/start?session_id=" + sessionId)
+                .cookie("JSESSIONID", jsessionId)
                 .method(Connection.Method.GET)
                 .execute();
 
@@ -88,7 +110,12 @@ public class StudidbService {
     }
 
     public List<ExamResult> getExamResults(StudidbAuthorization authorization) throws Exception {
-        Document doc = getExamResultsDocument(authorization);
-        return parseResults(doc);
+        Document doc = getStudidbDocument("/studierende/leistungen", authorization.sessionId(), authorization.jsessionId());
+        return parseExamResults(doc);
+    }
+
+    public StudidbUserInfo getUserInfo(StudidbAuthorization authorization) throws Exception {
+        Document doc = getStudidbDocument("/studierende/start", authorization.sessionId(), authorization.jsessionId());
+        return parseUserInfo(doc);
     }
 }
